1b3788623a6470e85e1d02b19c9c1f2b155bfc9f
Slowhand <eeeeeeediot@gmail.com>
2020-02-02 21:30:30 +0100

    Minor name changes and comments on QuestManager. Unpacked data.zip to be able to see changes better and not to need to use LSF.

---
+++ PReloaded/Client/data/art/critters/NMSUPA Readme.txt
@@ -0,0 +1,5 @@
+NMSUPA Notes
+
+1) I did not make G series (spear) for NMSUPA as i considered
+it and unlikely weapon at that stage of the game.  Use the generic
+HAPOWR GA-GM FRM's.
--- foclassic/Source/QuestManager.cpp
+++ foclassic/Source/QuestManager.cpp
@@ -12,6 +12,40 @@
 using namespace std;
 using namespace Script;
 
+/**
+	FOTEXT.MSG:
+
+	# Number formula as in FOTEXT.MSG file on server side.
+	#	VariableId * 1000 + Value (1-100) - The text that appears at a given value of the variable (current objective).
+	#	VariableId * 1000 + 101 - Name of quest tab.
+	#	VariableId * 1000 + 102 - Permanent quest description.
+
+	Example:
+
+	#=======================================
+	# 6502
+	# Landers Point: Caravan Packer
+	#=======================================
+
+	{6502001}{}{The caravan foreman at Landers Point asked me to unload some boxes from the cart to the north-west storage area. After unloading 3 boxes, my pay would be 250 caps.}
+	{6502004}{}{DONE:That's all. I should talk to the caravan foreman about my reward!}
+	{6502097}{}{SOFT_FAILED:I tried to help the caravan packer but I dropped one of the boxes on the wrong location. Maybe I should work out a bit before trying again.}
+	{6502099}{}{SOFT_COMPLETED:I finished the task and got my reward. I could return for more work a bit later.}
+	{6502101}{}{JOB:Landers Point Caravan packer}
+	{6502102}{}{Unload boxes from the caravan cart.}
+
+	C++ code:
+
+	Full variable (VAR) = {6502001}
+
+	questVarId			= 6502
+	questVarValue		= 001
+	tabName				= "JOB:Landers Point Caravan packer"
+
+	If VAR ends in 000, that quest is not any kind of progress, so it would be erased from pip boy if present before.
+
+*/
+
 /// QuestTab
 void QuestTab::ReparseText()
 {
@@ -89,62 +123,63 @@ void QuestManager::Finish()
     tabs.clear();
 }
 
-void QuestManager::OnQuest( uint num )
+void QuestManager::OnQuest( uint questVar )
 {
+
     // Split
-    uint16 q_num = num / QUEST_MUL;
-    uint16 val = num % QUEST_MUL;
+    uint16 questVarId = questVar / QUEST_MUL;
+    uint16 questVarValue = questVar % QUEST_MUL;
 
     // Check valid Name of Tab
-    if( !msg->Count( STR_QUEST_MAP_( q_num ) ) )
-        return;
-
-    // Get Name of Tab
-    string tab_name = string( msg->GetStr( STR_QUEST_MAP_( q_num ) ) );
+	if (!msg->Count(GET_QUEST_NAME_VAR(questVarId))) {
+		return;
+	}
+    string tabName = string( msg->GetStr( GET_QUEST_NAME_VAR( questVarId ) ) );
 
     // Try get Tab
     QuestTab* tab = NULL;
-    auto      it_tab = tabs.find( tab_name );
-    if( it_tab != tabs.end() )
-        tab = &(*it_tab).second;
+    auto it = tabs.find( tabName );
+	if (it != tabs.end()) {
+		tab = &(*it).second;
+	}
 
     // Try get Quest
     Quest* quest = NULL;
-    if( tab )
-        quest = tab->GetQuest( q_num );
-
-    // Erase	quest
-    if( !val )
+	if (tab) {
+		quest = tab->GetQuest(questVarId);
+	}
+		
+    // If quest variable value is 000, player is not aware of this quest, need to erase.
+    if( !questVarValue )
     {
         if( tab )
         {
-            tab->EraseQuest( q_num );
+            tab->EraseQuest( questVarId );
             if( tab->IsEmpty() )
-                tabs.erase( tab_name );
+                tabs.erase( tabName );
         }
         return;
     }
 
     // Add Tab if not exists
-    if( !tab )
-        tab = &(*(tabs.insert( PAIR( tab_name, QuestTab( msg ) ) ) ).first).second;
+	if (!tab) {
+		tab = &(* ( tabs.insert(PAIR(tabName, QuestTab(msg))) ).first ).second;
+	}
 
     // Add Quest if not exists
     if( !quest )
-        quest = tab->AddQuest( q_num, string( msg->GetStr( STR_QUEST_INFO_( q_num ) ) ) );
+        quest = tab->AddQuest( questVarId, string( msg->GetStr( GET_QUEST_INFO_VAR( questVarId ) ) ) );
 
     // Get name of quest
-    tab->RefreshQuest( q_num, string( msg->GetStr( num ) ) );
+    tab->RefreshQuest( questVarId, string( msg->GetStr( questVar ) ) );
 
+	//	Generate callback to AS at client when quest changed
 	if (Script::PrepareContext(ClientFunctions.QuestChange, _FUNC_, "Game"))
 	{
-		//string name = string(msg->GetStr(STR_QUEST_INFO_(q_num)));
-		string name = tab_name;
-
-		ScriptString* event_name_script = new ScriptString(name);
+		ScriptString* event_name_script = new ScriptString(tabName);
 		Script::SetArgObject(event_name_script);
 
-		string text = string(msg->GetStr(num));
+		string text = string(msg->GetStr(questVar));
 		ScriptString* event_text_script = new ScriptString(text);
 		Script::SetArgObject(event_text_script);
 
@@ -184,9 +219,9 @@ Quest* QuestManager::GetQuest( uint tab_num, uint16 quest_num )
 
 Quest* QuestManager::GetQuest( uint num )
 {
-    if( !msg->Count( STR_QUEST_MAP_( num / QUEST_MUL ) ) )
+    if( !msg->Count( GET_QUEST_NAME_VAR( num / QUEST_MUL ) ) )
         return NULL;
-    string tab_name = string( msg->GetStr( STR_QUEST_MAP_( num / QUEST_MUL ) ) );
+    string tab_name = string( msg->GetStr( GET_QUEST_NAME_VAR( num / QUEST_MUL ) ) );
     auto   it_tab = tabs.find( tab_name );
     return it_tab != tabs.end() ? (*it_tab).second.GetQuest( num / QUEST_MUL ) : NULL;
 }
